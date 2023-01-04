f0571e8c9971fbbd6cf502a76439be13f41a9ba8
matheusfdc10 <64614285+matheusfdc10@users.noreply.github.com>
2023-01-04 19:09:27 -0300

    at

--- src/server/api.js
+++ src/server/api.js
@@ -8,8 +8,3 @@ export const getUser = async (login, password) => {
     const url = `/api_usuario.php?acao=login&login=${login}&senha=${password}`
     return api.get(url)
 }
-
-// export const getUser = async (login, password) => {
-//     return fetch('http://claudiacorretor.myartsonline.com/api_usuario.php?acao=login&login=matheus&senha=matheus').then(res => res.json())
-//     // return axios.get('http://claudiacorretor.myartsonline.com/api_usuario.php?acao=login&login=matheus&senha=matheus')
-// }
